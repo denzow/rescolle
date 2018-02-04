f5ae826871916c0d9671f2f9d97df218cc52cd8e
denzow <denzow@gmail.com>
2018-02-04 22:13:52 +0900

    [add] model の整理

---
+++ rescolle_view/models/__init__.py
@@ -0,0 +1,4 @@
+# coding: utf-8
+
+from .crawl_raw_data import CrawlRawData
+from .gnavi_restaurant import GnaviRestaurant
--- rescolle_view/models/gnavi_restaurant.py
+++ rescolle_view/models/gnavi_restaurant.py
@@ -1,35 +1,4 @@
-
-import json
-
 from django.db import models
-from .constraints import SourceType
-
-
-class CrawlRawData(models.Model):
-
-    raw_json = models.TextField()
-    source = models.IntegerField()
-    crawled_at = models.DateTimeField(auto_now_add=True)
-    serial = models.TextField(blank=True, null=True, unique=True)
-
-    @classmethod
-    def get_by_serial(cls, serial):
-        try:
-            return cls.objects.get(serial=serial)
-        except cls.DoesNotExist:
-            return None
-
-    @property
-    def json(self):
-        return json.loads(self.raw_json)
-
-    @property
-    def source_type(self):
-        return SourceType(self.source)
-
-    def clear_serial(self):
-        self.serial = None
-        self.save()
 
 
 class GnaviRestaurant(models.Model):
