42542baa6efe6d450a0550c77def74141db2477c
denzow <denzow@gmail.com>
2018-03-24 21:53:52 +0900

    [modify] pipelineの小さなリファクタ

--- rescolle_crawler/rescolle_crawler/pipelines.py
+++ rescolle_crawler/rescolle_crawler/pipelines.py
@@ -7,10 +7,9 @@ import requests
 from .items import CrawlJsonItem
 
 
-class RestaurantCollectorPipeline(object):
+class RestaurantCollectorPipeline:
 
-    def __init__(self, output_base_dir, request_endpoint):
-        self.output_base_dir = output_base_dir
+    def __init__(self, request_endpoint):
         self.request_endpoint = request_endpoint
         self.tmp_item_list = []
 
@@ -21,27 +20,25 @@ class RestaurantCollectorPipeline(object):
         :return:
         """
         return cls(
-            output_base_dir=crawler.settings.get('GNAVI_RESTAURANT_OUTPUT_DATA_DIR'),
             request_endpoint=crawler.settings.get('RESCOLLE_SERVER_ENDPOINT'),
         )
 
     def open_spider(self, spider):
         pass
 
+    def process_item(self, item, spider):
+        """
+        item単位で処理はせず、貯めるだけにしている
+        :param item:
+        :param spider:
+        :return:
+        """
+        self.tmp_item_list.append(item)
+        return item
+
     def close_spider(self, spider):
         crawl_json_item = CrawlJsonItem.create(self.tmp_item_list)
         crawl_json_item.save()
-
-        # # 生データの保存
-        # with open(os.path.join(self.output_base_dir, '{}.json'.format(spider.name)), 'w') as f:
-        #     json.dump(self.tmp_item_list, f, indent=4, ensure_ascii=False)
-
         # 終了通知
         spider.logger.info('send finish message for {}'.format(crawl_json_item['serial']))
         requests.get(self.request_endpoint + crawl_json_item['serial'])
-
-    def process_item(self, item, spider):
-
-        # print('{} {} {}'.format(item['name'], item['latitude'], item['longitude']))
-        self.tmp_item_list.append(item)
-        return item
