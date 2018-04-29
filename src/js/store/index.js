271876a4dac06ccc9669b3b3bbf214d8c6d5348c
denzow <denzow@gmail.com>
2018-04-29 23:03:40 +0900

    [modify] storeの整理

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -9,109 +9,114 @@ import {
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
-    searchWord: '',
-    markers: [],
-    collectedMarkers: [],
-    collectionList: [],
-  },
-  actions: {
-    setMarkers(context, payload) {
-      context.commit('setMarkers', payload);
-    },
-    search(context, payload) {
-      let markers = [];
 
-      fetch('/get_coordinate_list', {
-        method: 'POST',
-        mode: 'same-origin',
-        credentials: 'include',
-        headers: {
-          'X-CSRFToken': getCookie('csrftoken'),
-          'Accept': 'application/json',
-        },
-        body: generateFormData(payload)
+const state = {
+  searchWord: '',
+  markers: [],
+  collectedMarkers: [],
+  collectionList: [],
+};
+
+const actions = {
+  setMarkers(context, payload) {
+    context.commit('setMarkers', payload);
+  },
+  search(context, payload) {
+    let markers = [];
+    fetch('/get_coordinate_list', {
+      method: 'POST',
+      mode: 'same-origin',
+      credentials: 'include',
+      headers: {
+        'X-CSRFToken': getCookie('csrftoken'),
+        'Accept': 'application/json',
+      },
+      body: generateFormData(payload)
+    })
+      .then(res => {
+        return res.json();
       })
-        .then(res => {
-          return res.json();
+      .then(json => {
+        for (let rest of json['restaurants']) {
+          markers.push({
+            position: {
+              lat: rest['latitude'],
+              lng: rest['longitude'],
+            },
+            restaurantId: rest['id'],
+          })
+        }
+        context.commit('setMarkers', {markers})
+      });
+  },
+  setSearchWord(context, payload) {
+    context.commit('setSearchWord', payload)
+  },
+  initCollection(context, payload){
+    console.log('initCollection');
+    fetch('/get_collection_list', {
+      method: 'GET',
+      mode: 'same-origin',
+      credentials: 'include',
+      headers: {
+        'X-CSRFToken': getCookie('csrftoken'),
+        'Accept': 'application/json',
+      },
+    })
+    .then(res => {
+      return res.json();
+    })
+    .then(json => {
+      context.commit('setCollection', json)
+    });
+  },
+  setCollectionToMark(context, payload){
+    const collectionId = payload.collectionId;
+    const markers = [];
+    fetch(`/get_coordinate_list/${collectionId}`, {
+      method: 'GET',
+      mode: 'same-origin',
+      credentials: 'include',
+      headers: {
+        'X-CSRFToken': getCookie('csrftoken'),
+        'Accept': 'application/json',
+      },
+    })
+    .then(res => {
+      return res.json();
+    })
+    .then(json => {
+      for (let rest of json['restaurants']) {
+        markers.push({
+          position: {
+            lat: rest['latitude'],
+            lng: rest['longitude'],
+          },
+          restaurantId: rest['id'],
         })
-        .then(json => {
+      }
+      context.commit('setCollectedMarkers', {markers});
+    });
+  }
+};
+const mutations = {
+  setMarkers(state, payload) {
+    state.markers = payload.markers;
+  },
+  setSearchWord(state, payload) {
+    state.searchWord = payload.searchWord;
+  },
+  setCollection(state, payload) {
+    state.collectionList = payload.collection_list;
+  },
+  setCollectedMarkers(state, payload){
+    state.collectedMarkers = payload.markers;
+  }
+};
 
-          for (let rest of json['restaurants']) {
-            markers.push({
-              position: {
-                lat: rest['latitude'],
-                lng: rest['longitude'],
-              },
-              restaurantId: rest['id'],
-            })
-          }
-          context.commit('setMarkers', {markers})
-        });
-    },
-    setSearchWord(context, payload) {
-      context.commit('setSearchWord', payload)
-    },
-    initCollection(context, payload){
-      console.log('initCollection');
-      fetch('/get_collection_list', {
-        method: 'GET',
-        mode: 'same-origin',
-        credentials: 'include',
-        headers: {
-          'X-CSRFToken': getCookie('csrftoken'),
-          'Accept': 'application/json',
-        },
-      })
-      .then(res => {
-        return res.json();
-      })
-      .then(json => {
-        context.commit('setCollection', json)
-      });
-    },
-    setCollectionToMark(context, payload){
-      const collectionId = payload.collectionId;
-      const markers = [];
-      fetch(`/get_coordinate_list/${collectionId}`, {
-        method: 'GET',
-        mode: 'same-origin',
-        credentials: 'include',
-        headers: {
-          'X-CSRFToken': getCookie('csrftoken'),
-          'Accept': 'application/json',
-        },
-      })
-      .then(res => {
-        return res.json();
-      })
-      .then(json => {
-          for (let rest of json['restaurants']) {
-            markers.push({
-              position: {
-                lat: rest['latitude'],
-                lng: rest['longitude'],
-              },
-              restaurantId: rest['id'],
-            })
-          }
-          context.commit('setCollectedMarkers', {markers});
-      });
-    }
-  },
-  mutations: {
-    setMarkers(state, payload) {
-      state.markers = payload.markers;
-    },
-    setSearchWord(state, payload) {
-      state.searchWord = payload.searchWord;
-    },
-    setCollection(state, payload) {
-      state.collectionList = payload.collection_list;
-    },
-    setCollectedMarkers(state, payload){
-      state.collectedMarkers = payload.markers;
-    }
-  },
+
+export default new Vuex.Store({
+  state,
+  actions,
+  mutations,
 });
